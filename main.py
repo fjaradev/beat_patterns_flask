5e6906cdb243dae18809d97582c23b3a3ade51b1
Fernando Jara <fjaradevelopment@gmail.com>
2026-03-26 12:16:21 -0400

    I realized that using a class instance just centralizes the state on the server so every device will modify the general state so it will be updated on every device. That is not the behavior that I was expecting so I modified the main.py to use session objects for every device.

--- main.py
+++ main.py
@@ -1,44 +1,36 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from typing import List, Annotated
 from random import choice, choices
 
 app = Flask(__name__)
+app.secret_key = "Asldkfu82348AHSDLf82834laSDFLu#oayf"
 
-class Sequencer:
-    
-    """
-        Class that makes an object with the pattern that will be generated.
-    """
-    
-    def __init__(self):
-        self.pattern: List[int] = []
-        self.pattern_num_bars: int = 4
-        self.active_subdivisions: List[int] = [1, 2, 3, 4]
-        
-    def generate_pattern(self):
+def generate_pattern(active_subdivisions: list[int], number_bars: int) -> list[int]:
         """
             Generates a new array with random numbers between 1 and 4.\n
             It modifies the instance pattern attribute with the new array and clears the previous one.\n
             The size of the pattern will be defined by the pattern_num_bars.
         """
         
-        self.pattern.clear()
-        for _ in range(4 * self.pattern_num_bars):
-            self.pattern.append(choice(self.active_subdivisions))
-            
-
-sequencer = Sequencer()
+        new_pattern = []
+        for _ in range(4 * number_bars):
+            new_pattern.append(choice(active_subdivisions))
+        
+        return new_pattern
 
 
 @app.route("/")
 def home():
-    sequencer.generate_pattern()
-    return render_template("index.html", pattern=sequencer.pattern)
+    session["num_bars"] = 4
+    if not "active_subdivisions" in session:
+        session["active_subdivisions"] = [1, 2, 3, 4]
+    session["pattern"] = generate_pattern(active_subdivisions=session["active_subdivisions"], number_bars=session["num_bars"])
+    return render_template("index.html", pattern=session["pattern"])
 
 @app.route("/generate-pattern", methods=["GET", "POST"])
-def generate_pattern():
-    sequencer.generate_pattern()
-    return render_template("beat_pattern.html", pattern=sequencer.pattern)
+def generate_new_pattern():
+    session["pattern"] = generate_pattern(active_subdivisions=session["active_subdivisions"], number_bars=session["num_bars"])
+    return render_template("beat_pattern.html", pattern=session["pattern"])
 
 @app.route("/app-set-up")
 def app_set_up():
@@ -47,7 +39,8 @@ def app_set_up():
 @app.route("/confirm-selection", methods=["GET", "POST"])
 def confirm_selection():
     if request.method == "POST":
-        sequencer.active_subdivisions = request.form.getlist("selected_ids")
+        print(session["active_subdivisions"])
+        session["active_subdivisions"] = request.form.getlist("selected_ids")
     return redirect("/", code=303)
     
 if __name__ == "__main__":
